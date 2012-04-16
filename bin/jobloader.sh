6782d12cd9b1d6d66262ccdd4d1a448063ba1f09
Henrik Lynggaard Hansen <henrik@hlyh.dk>
2012-04-16 22:44:44 +0200

    debug output

--- bin/jobloader.sh
+++ bin/jobloader.sh
@@ -6,4 +6,5 @@ CLASSPATH=$BASE_DIR/lib/hudson-cli.jar:$BASE_DIR/lib/jobcreator.jar:$BASE_DIR/li
 
 rm -rf $OUTPUT_DIR
 mkdir $OUTPUT_DIR
+echo $CLASSPATH
 java -cp $CLASSPATH org.python.util.jython ./jobLoader.py $1 $2 $3 $4
